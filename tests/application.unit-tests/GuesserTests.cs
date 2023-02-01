892602be99c250c11df0b5131589f2e5f45c5851
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-02-01 17:50:28 +0300

    made guesser static

--- tests/application.unit-tests/GuesserTests.cs
+++ tests/application.unit-tests/GuesserTests.cs
@@ -4,28 +4,12 @@ namespace application.unit_tests;
 
 public class GuesserTests
 {
-    private Guesser guesser;
-
-    [SetUp]
-    public void Setup()
-    {
-        guesser = new Guesser();
-    }
-
     [Test]
     public void GuessFirstWordCorrectly()
     {
-        var result = guesser.GuessFirstWord();
+        var result = Guesser.GuessFirstWord();
         result.Should().NotBeEmpty("First guess shold return word");
         Console.WriteLine(result);
-    }
-
-    [Test]
-    public void GuessFirstWordTwiceFails()
-    {
-        guesser.GuessFirstWord();
-        var secondGuess = () => guesser.GuessFirstWord();
-        secondGuess.Should().Throw<InvalidOperationException>().WithMessage("You cant guess first word on round N 1");
     }
 
     [Test]
@@ -35,7 +19,7 @@ public class GuesserTests
         word.AddWrongLetters("аоедк");
         word.AddInappropriateLetters("    ь");
         word.AddCorrectLetters(" и   ");
-        guesser.GuessFirstWord();
-        guesser.GuessNextWord(word);
+        Guesser.GuessFirstWord();
+        Guesser.GuessNextWord(word);
     }
 }
